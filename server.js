d8873c6b45cc43f6e9674f7a81484ded766ebcd3
Matheusaraujo007 <159270260+Matheusaraujo007@users.noreply.github.com>
2025-07-22 18:21:01 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // --- Rota principal: formulário de novo pedido ---
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'vendedor.html'));
 });
 
 // --- Cadastrar pedido ---
